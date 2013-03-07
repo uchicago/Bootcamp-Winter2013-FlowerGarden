fb7a004d6bb9b43c7debfdcc18a0f2fee96ab5f9
T. Andrew Binkowski <abinkowski@uchicago.edu>
2013-03-07 12:56:54 -0600

    Add code to place flower image at touch point

--- FlowerGarden/ViewController.m
+++ FlowerGarden/ViewController.m
@@ -26,7 +26,35 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)tapToAddFlower:(UITapGestureRecognizer *)sender {
-    NSLog(@">>>>> tapToAddFlower:");
+#pragma mark Gesture Handling
+
+/*******************************************************************************
+ * @method          tapToAddFlower:
+ * @abstract        Action for single tap gesture recognizer
+ * @description     Add an UIImage to the parentview 
+ *******************************************************************************/
+- (IBAction)tapToAddFlower:(UITapGestureRecognizer *)sender
+{
+    NSLog(@">>>>> tapToAddFlower: %@", sender);
+
+    // Get the view that was tapped
+    UIView *field = sender.view;
+
+    // Get the location of the tap on the field view
+    CGPoint touchPoint = [sender locationInView:[field superview]];
+    NSLog(@"\t\t Field was tapped at location: x:%5.2f y:%5.2f",touchPoint.x,touchPoint.y);
+    
+    // Create an image to add to the field view
+    UIImage *flower = [UIImage imageNamed:@"red"];
+    
+    // Add an image view wit
+    UIImageView *flowerImageView = [[UIImageView alloc] initWithImage:flower];
+    
+    // Place it at the touch point
+    flowerImageView.center = touchPoint;
+    
+    // Add our image view to the field view
+    [field addSubview:flowerImageView];
 }
+
 @end
